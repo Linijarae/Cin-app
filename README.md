cc48445400d13928c5831ccfbd7a64554f4c84e6
Linijarae <alexis.dupin@ynov.com>
2026-02-05 16:18:10 +0100

    authController and index.php

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Cin-app
 Une petite application web en PHP, pour réserver vos places de cinéma. Manipulation de cookies de sessions, de cookies utilisateurs et sécurisation.
+
+
+Pour lancer : php -S localhost:8000 index.php
--- src/controllers/authController.php
+++ src/controllers/authController.php
@@ -1,2 +1,21 @@
 <?php
 
+require_once 'models/cineModel.php';
+
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . $path)) {
+    return false; 
+}
+
+$uri = $_SERVER['REQUEST_URI'];
+$path = parse_url($uri, PHP_URL_PATH);
+switch ($path) {
+    case '/':
+        $moviesModel = new MoviesModel();
+        $movies = $moviesModel->getAllMovies();
+        require 'index.php';
+        break;
+    default:
+        http_response_code(404);
+        require 'pages/404.php';
+        break;
+}
