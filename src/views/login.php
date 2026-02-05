b75d476226a2dc76e725eb901972eeef61b8f96c
Adrien LECOMTE <97622219+ZdarkBlackShadow@users.noreply.github.com>
2026-02-05 15:53:33 +0100

    add logo and views file

---
+++ src/views/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <link rel="stylesheet" href="../../public/css/login.css">
+</head>
+<body>
+    <header>
+        <img src="../../public/logo.png" alt="Logo">
+    </header>
+    <form action="/login" method="POST">
+        <label for="email"></label>
+        <input type="mail" name="email" id="email" placeholder="email">
+        <label for="password"></label>
+        <input type="password" name="password" id="password" placeholder="password">
+        <button type="submit">Login</button>
+    </form>
+    <footer>
+        
+    </footer>
+</body>
+</html>
