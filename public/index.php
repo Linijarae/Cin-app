5769342809de32930d99a754447453769d9af6de
Linijarae <alexis.dupin@ynov.com>
2026-02-05 16:23:34 +0100

    index.php basic route

--- public/index.php
+++ public/index.php
@@ -1,2 +1,26 @@
 <?php
 
+$uri = $_SERVER['REQUEST_URI'];
+$path = parse_url($uri, PHP_URL_PATH);
+
+switch ($path) {
+    case '/':
+    case '/home':
+        $titre = "Bienvenue"; 
+        require 'views/home.php';
+        break;
+
+    case '/login':
+        $titre = "Log in here";
+        require 'views/login.php';
+        break;
+
+    case '/profile':
+        require 'views/profile.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require 'pages/404.php';
+        break;
+}
