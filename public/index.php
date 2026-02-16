ebd47e81bfa11c501119c6de0ffcbc0f5605410f
Linijarae <alexis.dupin@ynov.com>
2026-02-16 14:10:37 +0100

    Correctifs

--- public/index.php
+++ public/index.php
@@ -19,7 +19,6 @@ switch ($path) {
 
     case '/login':
         $titre = "Log in here";
-<<<<<<< HEAD
         require './src/views/login.php';
         break;
 
@@ -41,21 +40,10 @@ switch ($path) {
     case '/cgu':
         $titre = "CGU";
         require './src/views/cgu.php';
-=======
-        require '../src/views/login.php';
-        break;
-
-    case '/profile':
-        require '../src/views/profile.php';
->>>>>>> a882fad641f2690dfc559ce065643c4dc5954030
         break;
 
     default:
         http_response_code(404);
-<<<<<<< HEAD
         require './src/views/404.php';
-=======
-        require '../src/views/404.php';
->>>>>>> a882fad641f2690dfc559ce065643c4dc5954030
         break;
 }
